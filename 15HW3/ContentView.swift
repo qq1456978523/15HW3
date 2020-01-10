b2da88ce79040ee7b85610caa1fe7b0edcf923b0
Lawrenace <00657140@email.ntou.edu.tw>
2020-01-10 11:52:37 +0800

    Initial Commit

---
+++ 15HW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  15HW3
+//
+//  Created by User22 on 2020/1/10.
+//  Copyright © 2020 Lawrence. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
